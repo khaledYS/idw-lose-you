322235b984f6219725d367bbdc158f4dd7693285
admin@ammar <admin@ammar.awash>
2021-07-19 10:28:02 +0300

    updated

---
+++ public/index.js
@@ -0,0 +1,28 @@
+window.onresize = updateHeight;
+function updateHeight(){
+    document.body.style.height = `${window.innerHeight}px`
+}
+updateHeight()
+
+
+
+
+/* 
+
+body{
+    div.heart{
+        span{
+            lorem...
+        }
+    }
+    div.intro{
+        span{
+            span.firstWords{
+                lorem...
+            }
+            lorem...
+        }
+    }
+}
+
+*/
